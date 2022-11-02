31c4cbb3cf59f82fa4d5481d32787deb3cbf114a
WILLIAM <U072092@bca.co.id>
2022-11-02 11:37:44 +0700

    can copy and delete file from server to local

--- src/main/java/com/example/client/donwload/Client/Download/File/controller/RestClientController.java
+++ src/main/java/com/example/client/donwload/Client/Download/File/controller/RestClientController.java
@@ -1,9 +1,10 @@
 package com.example.client.donwload.Client.Download.File.controller;
 import com.example.client.donwload.Client.Download.File.service.RestClientService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
 
 @Controller
 public class RestClientController {
@@ -12,7 +13,11 @@ public class RestClientController {
 
     //EVERY 1 MINUTES ON WEEKDAYS
     @Scheduled(cron = "0 */1 * * * MON-FRI")
-     public void getDataAll() throws JsonProcessingException {
-         restClientService.getRawDataAtts();
+    public void downloadFileIts() throws IOException {
+        restClientService.getItsFile();
+    }
+    @Scheduled(cron = "0 */1 * * * MON-FRI")
+    public void downloadFileJts() throws IOException {
+        restClientService.getJtsFile();
     }
 }
--- src/main/java/com/example/client/donwload/Client/Download/File/service/RestClientService.java
+++ src/main/java/com/example/client/donwload/Client/Download/File/service/RestClientService.java
@@ -1,15 +1,12 @@
 package com.example.client.donwload.Client.Download.File.service;
-import com.example.client.donwload.Client.Download.File.dto.Attachments;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Base64;
+import java.util.stream.Stream;
 
 @Service
 public class RestClientService {
@@ -17,25 +14,105 @@ public class RestClientService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void getRawDataAtts() {
+//    public void getDataJts() {
+//
+//        ResponseEntity<Attachments> response = restTemplate.getForEntity("http://localhost:8081/api/download/jts", Attachments.class);
+//        Attachments responseAttcs = response.getBody();
+//
+//        responseAttcs
+//                .getAttachments()
+//                .stream()
+//                .forEach(
+//                        attachment -> {
+//                            byte[] decodedImg = Base64.getDecoder().decode(attachment.getFileBytes().getBytes(StandardCharsets.UTF_8));
+//                            Path destinationFile = Paths.get("C:\\WILLIAM\\00.IBO-D\\Test_Download_File\\JTS\\", attachment.getFileName());
+//                            try {
+//                                Files.write(destinationFile, decodedImg);
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//                );
+//        System.out.println("Download JTS File Done");
+//    }
 
-        ResponseEntity<Attachments> response = restTemplate.getForEntity("http://localhost:8081/api/download/jts", Attachments.class);
-        Attachments responseAttcs = response.getBody();
+    public void getItsFile() {
+        String src = "\\\\10.20.201.111\\c\\apiprintertest\\ITS\\";
+        String dest = "C:\\WILLIAM\\00.IBO-D\\Test_Download_File\\ITS\\";
 
-        responseAttcs
-                .getAttachments()
-                .stream()
-                .forEach(
-                    attachment -> {
-                        byte[] decodedImg = Base64.getDecoder().decode(attachment.getFileBytes().getBytes(StandardCharsets.UTF_8));
-                        Path destinationFile = Paths.get("C:\\WILLIAM\\00.IBO-D\\DOWNLOADYUHU\\", attachment.getFileName());
+        try (Stream<Path> paths = Files.walk(Paths.get(src))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
                         try {
-                            Files.write(destinationFile, decodedImg);
+                            Files.copy(path, Path.of(dest + path.getFileName()));
+//                            System.out.println("path : " + path);
+//                            System.out.println("dest: " + dest + path.getFileName());
+                            Files.delete(Path.of(src + path.getFileName()));
                         } catch (IOException e) {
-                            e.printStackTrace();
+                            throw new RuntimeException(e);
                         }
-                    }
-            );
-        System.out.println("Download procees done");
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    public void getJtsFile() {
+        String src = "\\\\10.20.201.111\\c\\apiprintertest\\JTS\\";
+        String dest = "C:\\WILLIAM\\00.IBO-D\\Test_Download_File\\JTS\\";
+
+        try (Stream<Path> paths = Files.walk(Paths.get(src))) {
+            paths
+                    .filter(Files::isRegularFile)
+                    .forEach(path -> {
+                        try {
+                            Files.copy(path, Path.of(dest + path.getFileName()));
+//                            System.out.println("path : " + path);
+//                            System.out.println("dest: " + dest + path.getFileName());
+                            Files.delete(Path.of(src + path.getFileName()));
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+//    public void getDataIts() throws InterruptedException {
+//
+//        ResponseEntity<Attachments> response = restTemplate.getForEntity("http://localhost:8081/api/download/its", Attachments.class);
+//        Attachments responseAttcs = response.getBody();
+//
+//        responseAttcs
+//                .getAttachments()
+//                .stream()
+//                .forEach(
+//                        attachment -> {
+//                            byte[] decodedImg = Base64.getDecoder().decode(attachment.getFileBytes().getBytes(StandardCharsets.UTF_8));
+////                            Path destinationFile = Paths.get("C:\\WILLIAM\\00.IBO-D\\Test_Download_File\\ITS\\", attachment.getFileName());
+//                            try {
+//                                //INI BISA
+////                                FileOutputStream files = new FileOutputStream("C:/WILLIAM/00.IBO-D/Test_Download_File/ITS/" + attachment.getFileName());
+//
+//                                //INI JUGA BISA
+//                                    Files.write(Path.of("C:/WILLIAM/00.IBO-D/Test_Download_File/ITS/" + attachment.getFileName()), decodedImg);
+//
+//                            } catch (FileNotFoundException e) {
+//                                throw new RuntimeException(e);
+//                            } catch (IOException e) {
+//                                throw new RuntimeException(e);
+//                            }
+//                        }
+//                );
+//
+//        System.out.println("Download ITS File Done");
+//    }
+//
+//    public ResponseEntity<Boolean> stopFunction() {
+//        ResponseEntity<Boolean> response = restTemplate.getForEntity("http://localhost:8081/api/stop", Boolean.class);
+//        System.out.println("Stopping from client...");
+//        return response;
+//    }
 }
